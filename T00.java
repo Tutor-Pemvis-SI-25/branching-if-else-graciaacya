be9fdc39c190bf828bdd962ec4aa1a102fd883c6
Gracia Hutagaol <147119426+graciaacya@users.noreply.github.com>
2025-10-03 14:36:49 +0700

    Pair 08
    Selesai

--- T00.java
+++ T00.java
@@ -10,41 +10,37 @@ public class T00 {
         a = Integer.parseInt(input.nextLine());
         b = Integer.parseInt(input.nextLine());
         if (a % 2 == 0) {
-            System.out.println("a adalah genap");
+            System.out.println("Bilangan pertama: Genap");
         } else {
-            System.out.println("a adalah ganjil");
+            System.out.println("Bilangan pertama: Ganjil");
         }
         if (b % 2 == 0) {
-            System.out.println("b adalah genap");
+            System.out.println("Bilangan kedua: Genap");
         } else {
-            System.out.println("b adalah ganjil");
+            System.out.println("Bilangan kedua: Ganjil");
         }
-        if (a == b) {
-            System.out.println("sama besar");
+        if (a > b) {
+            System.out.println("Bilangan pertama lebih besar");
         } else {
-            if (a > b) {
-                System.out.println("a lebih besar");
+            if (a < b) {
+                System.out.println("Bilangan kedua lebih besar");
             } else {
-                if (b > a) {
-                    System.out.println("b lebih besar");
-                } else {
-                    System.out.println("keduanya sama besar");
-                }
+                System.out.println("Kedua bilangan sama besar");
             }
         }
         if (a % 2 == 0) {
             if (b % 2 == 0) {
                 tambah = a + b;
-                System.out.println(tambah);
+                System.out.println("Hasil penjumlahan: " + tambah);
             } else {
-                System.out.println("berbeda jenis");
+                System.out.println("Berbeda jenis");
             }
         } else {
             if (b % 2 != 0) {
                 kali = a * b;
-                System.out.println(kali);
+                System.out.println("Hasil perkalian: " + kali);
             } else {
-                System.out.println("berbeda jenis");
+                System.out.println("Berbeda jenis");
             }
         }
     }
